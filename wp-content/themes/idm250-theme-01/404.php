0a5264d4ee13046ce9c555e096eb3e654fe060a9
cindyqquach <cq63@drexel.edu>
2023-03-13 01:17:58 -0400

    :construction:
    404 Page

---
+++ wp-content/themes/idm250-theme-01/404.php
@@ -0,0 +1,11 @@
+<?php get_header('404'); ?>
+<div>
+      <p>404</p>
+      <h1>This page does not exist</h1>
+      <p>Sorry, we couldn&#39;t find the page you&#39;re looking for.</p>
+</div>
+<div>
+    <button><a href="#">Back to home</a></button> 
+</div>
+<?php get_footer('simple'); ?>
+
---
+++ wp-content/themes/idm250-theme-01/components/footer-simple.php
@@ -0,0 +1,10 @@
+
+<footer>
+  <div>
+    <p>&copy;
+      <?php echo date('Y'); ?>
+      PAWS.
+      All rights reserved.
+    </p>
+  </div>
+</footer>

351fcbe0e4bcdeae2b6ef09c47ec3d4eae116344
cindyqquach <cq63@drexel.edu>
2023-03-21 04:08:56 -0400

    :construction:
    update main.css

--- wp-content/themes/idm250-theme-01/templates/full-width.php
+++ wp-content/themes/idm250-theme-01/templates/full-width.php
@@ -2,11 +2,6 @@
 /* Template Name: Full Width */
 get_header();
 ?>
-    <div>
-      <h2>
-        <?php echo get_the_title(); ?>
-      </h2>
-    </div>
   <div>
     <?php get_template_part('components/content'); ?>
   </div>

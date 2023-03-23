29fadb10d2def05c689039f820d3ccbe681c138d
cindyqquach <cq63@drexel.edu>
2023-03-23 06:17:31 -0400

    :construction:
    finish styling homepage.php

--- wp-content/themes/idm250-theme-01/front-page.php
+++ wp-content/themes/idm250-theme-01/front-page.php
@@ -1,11 +1,14 @@
 <?php get_header(); ?>
 <h1><?php echo get_the_title(); ?></h1>
-<?php 
-// check if the post or page has a featured image assigned to it 
-if (has_post_thumbnail()) {
-    the_post_thumbnail(); 
-}
-?> 
+<div> 
+    <?php echo get_the_title(); ?> 
+    <?php if (get_the_post_thumbnail_url()) {
+      echo 
+      "<div>
+      <img src='" . get_the_post_thumbnail_url() . "' alt='Hero Image'>
+      </div>";
+  } ?>
+</div> 
 <?php get_template_part('components/content'); ?>
 <?php get_footer(); ?>
 
